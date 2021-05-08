dd3a82d5813f4a334f507b60782271f716a20907
AllPlayer <ayushparida999@gmail.com>
2021-05-08 23:11:52 +0530

    Implementing Highcharts with Plotting of Chart for Checkboxes from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,94 @@
-import { Component, OnInit } from '@angular/core';
-import { MainService } from './main.service'
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { MainService } from './main.service';
+import * as Highcharts from 'highcharts';
+import { Chart, StockChart } from 'angular-highcharts';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'yumemi-task';
-  prefectures:any={};
+  form: FormGroup;
+  prefectures: any = [];
+  selected: any = [];
+  public chart: Chart = new Chart();
+  public highChartsOptions: Highcharts.Options = {};
 
-  constructor(private mainService: MainService) { }
+  constructor( private mainService: MainService, private formBuilder: FormBuilder ) {
+    this.form = this.formBuilder.group({
+      checked: this.formBuilder.array([], [Validators.required])
+    });
+  }
 
   ngOnInit(): void {
     this.mainService
-      .prefectureApi()
-      .subscribe(
-        data => {
-            this.prefectures=data;
-        },
-        err => console.log(err));
+        .prefectureApi()
+        .then(
+          data => {
+              this.prefectures = data.result;
+          },
+          err => console.log(err));
+  }
+
+  ngAfterViewInit(): void {
+    this.loadChart(this.selected);
+    this.chart = new Chart(this.highChartsOptions);
+  }
+
+  loadChart(data: any): any{
+    this.highChartsOptions = {
+      chart: { type: 'line' },
+      title: { text: '' },
+      plotOptions: { series: { dataLabels: { enabled: true } } },
+      xAxis: {
+        categories:[ '1960', '1965', '1970', '1975', '1980', '1985', '1990', '1995', '2000', '2005',
+              '2010', '2015', '2020', '2025', '2030', '2035', '2040', '2045', '2050'],
+        title: {
+          text: '<b>Years</b>',
+          align: 'high'
+        }
+      },
+      yAxis: {
+        title: {
+          text: '<b>Population Count</b>',
+          align: 'middle'
+        }
+      },
+      series: data
+    }
+  }
+
+  onCheckboxChange(e: any): any {
+    this.chart = new Chart(this.highChartsOptions);
+
+    const checked: FormArray = this.form.get('checked') as FormArray;
+    if (e.target.checked) {
+      checked.push(new FormControl(e.target.value));
+      this.mainService
+        .populationByPrefectureApi(e.target.value)
+        .subscribe(
+          data => {
+            this.selected.push(Object.assign(this.prefectures.find( (record: any) => record.prefCode === Number(e.target.value)), data));
+            this.chart = new Chart(this.highChartsOptions);
+          },
+          err => console.log(err));
+    } else {
+       const index = checked.controls.findIndex(x => x.value === e.target.value);
+       checked.removeAt(index);
+       this.selected = this.remove(this.selected, this.selected.find( (record: any) => record.prefCode === Number(e.target.value)));
+       this.chart = new Chart(this.highChartsOptions);
+    }
+  }
+
+  remove(array: any, n: any): any
+  {
+    const index = array.indexOf(n);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+    return array;
   }
 }
--- src/app/main.service.ts
+++ src/app/main.service.ts
@@ -1,28 +1,53 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
   prefecturePath = "https://opendata.resas-portal.go.jp/api/v1/prefectures";
+  populationByPrefecturePath = "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear";
   constructor(private http: HttpClient) { }
 
-  prefectureApi() {
+  async prefectureApi() {
     const headers = { 'X-API-KEY':'g2bRHUL3xlMetlbfpZGr0bA0s2MvDWomHz3zSjlA' }
      return this.http.get<any>(this.prefecturePath, { headers })
-    //  .subscribe(data => {
-    //     console.log(data);
-    //     return data;
-    // })
-    // return this.http.get<any>(this.prefecturePath, { })
-    // .pipe(
-    //   map((res) => {
-    //     return res;
-    //   }),
-    //   catchError((err) => {
-    //     return err;
-    //   })
-    // )
+    .pipe(
+      map((res) => {
+        for (var i = 0; i < res.result.length; i++){
+          res.result[i].name = res.result[i].prefName;
+        }
+        return res;
+      }),
+      catchError((err) => {
+        return err;
+      })
+    ).toPromise()
+  }
+
+  populationByPrefectureApi( prefCode:any ){
+    const headers = { 'X-API-KEY':'g2bRHUL3xlMetlbfpZGr0bA0s2MvDWomHz3zSjlA' }
+     return this.http.get<any>(this.populationByPrefecturePath+'?prefCode='+prefCode+'&cityCode=-', { headers })
+    .pipe(
+      map((res) => {
+        var out = res.result.data.find( (record:any) => record.label == "総人口")||{data:null}
+        var result={
+          year:[],
+          data:[]
+        } as any;
+        if(out.data!=null){
+          for (var i = 0; i < out.data.length; i++) {
+            result.year.push(out.data[i].year)
+            result.data.push(out.data[i].value)
+          }
+        }
+        return result;
+      }),
+      catchError((err) => {
+        return err;
+      })
+    )
   }
 }
