81c969b07b912a1cfeb253dc003aabf933c2b856
AllPlayer <ayushparida999@gmail.com>
2021-05-08 13:49:47 +0530

    Project Init, Title Bar, Prefectures Fetch and Display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { MainService } from './main.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'yumemi-task';
+  prefectures:any={};
+
+  constructor(private mainService: MainService) { }
+
+  ngOnInit(): void {
+    this.mainService
+      .prefectureApi()
+      .subscribe(
+        data => {
+            this.prefectures=data;
+        },
+        err => console.log(err));
+  }
 }
